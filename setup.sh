b138c6998018d85a4bf15a531b393713c64783cd
Connor Bray <connor.bray@tylertech.com>
2022-06-17 21:09:24 +0000

    Added files to correct dir and copy

--- setup.sh
+++ setup.sh
@@ -8,8 +8,14 @@ chmod +x src/crun
 mv src/crun /mnt/user-data/usr/local/bin/crun
 mv src/tums /mnt/user-data/usr/local/bin/tums
 
+cp /mnt/user-data/usr/local/bin/tums /usr/local/bin/tums
+cp /mnt/user-data/usr/local/bin/crun /usr/local/bin/crun
+
 # Run setup.py script
 python3 src/setup.py
+
+# Copy config file to non-mounted directory
+cp /mnt/user-data/usr/local/etc/crun_config.json /usr/local/etc/crun_config.json 
 
 # Clean files
 rm -rf src/
