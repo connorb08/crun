93869a00a554d35a56a642504beffaa5ac6f1832
Connor Bray <connor.bray@tylertech.com>
2022-06-22 12:50:53 +0000

    Add debugging / logging

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@
 # Set file permissions to be executable
 chmod +x src/tums
 chmod +x src/crun
+chmod +x src/gaslog
 
 # Move files to correct directory
 mv src/crun /mnt/user-data/usr/local/bin/crun
 mv src/tums /mnt/user-data/usr/local/bin/tums
+mv src/gaslog /mnt/user-data/usr/local/bin/gaslog
 
+# Copy files to non-mounted directory
 cp /mnt/user-data/usr/local/bin/tums /usr/local/bin/tums
 cp /mnt/user-data/usr/local/bin/crun /usr/local/bin/crun
+cp /mnt/user-data/usr/local/bin/gaslog /usr/local/bin/gaslog
 
 # Run setup.py script
 python3 src/setup.py
@@ -19,4 +23,5 @@ cp /mnt/user-data/usr/local/etc/crun_config.json /usr/local/etc/crun_config.json
 
 # Clean files
 rm -rf src/
+rm -rf github/
 rm -- $0
